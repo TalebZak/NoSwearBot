b6cbcd5bff0af8751b8c0e240d9647f69e678cc4
Zakaria Taleb <63817418+TalebZak@users.noreply.github.com>
2020-09-27 21:14:46 +0100

    Commented my utils.py

--- utils.py
+++ utils.py
@@ -1,5 +1,11 @@
 def preprocessing(substring):
-    pi_table = [0]
+    """This function does the preprocessing for the KMP algorithm.
+       It checks the previous occurence of each letter and maps it to a pi_table variable that contains a tuple for each letter
+       Args:
+            substring: A string that represents the needle that we will be looking for
+       Return:
+            pi_table: A list in which we mapped each letter in the substring and if it occured we give it its previous occurence otherwise it takes a 0 value"""
+    pi_table = [0]#the first value is 0 to facilitate the kmp process
     first_occurrence = {}
     for i in range(len(substring)):
         letter = substring[i]
@@ -12,6 +18,13 @@ def preprocessing(substring):
 
 
 def is_substring(haystack, needle):
+    """This function checks whether a word is a substring of the other through the KMP string matching algorithm
+        This algorithm is faster than most string matching algorithm which allows the bot to be faster when checking for the swear word
+        Args:
+            haystack: The big string that we will be searching
+            needle: The target that we will be seeking
+        Return:
+            A boolean to confirm whether a word is in the string or not"""
     if len(needle) > len(haystack):
         return False
     reference = preprocessing(needle)
@@ -23,9 +36,11 @@ def is_substring(haystack, needle):
             if letter_pair[0] == haystack[i]:
                 j += 1
                 if j == len(needle):
+                    #if we get to the end of the string
                     return True
                 break
             else:
+                #returns j to the value in which the letter previously occured
                 j = reference[j][1]
         if j == 0 and reference[1][0] == haystack[i]:
             j += 1
